7d8fd57166d09748f2ea02e4780b5640486a2ffd
Derrick Liu <derrick56007@hotmail.com>
2017-05-11 17:33:44 -0700

    remove extra brush code

--- web/brush.dart
+++ web/brush.dart
@@ -3,32 +3,9 @@ import 'common/point.dart';
 class Brush {
   String color = '#000000';
   int size = 5;
-
   bool pressed = false;
   bool moved = false;
-//
   Point pos = new Point(0, 0);
-//  Point prevPos = new Point.zero();
 
   Brush();
-//
-//  factory Brush.fromJson(String json) {
-//    Map map = JSON.decode(json) as Map;
-//
-//    return new Brush()
-//      ..color = map['color']
-//      ..size = map['size']
-//      ..pressed = map['pressed']
-//      ..moved = map['moved']
-//      ..pos = new Point.fromList(map['pos'])
-//      ..prevPos = new Point.fromList(map['prevPos']);
-//  }
-//  String toJson() => JSON.encode({
-//        'color': color,
-//        'size': size,
-//        'pressed': pressed,
-//        'moved': moved,
-//        'pos': pos.toJson(),
-//        'prevPos': prevPos.toJson()
-//      });
 }
