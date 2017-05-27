1ed7dde3e4af9ac76fde13c713d5f1a2ac7caa3c
Derrick Liu <derrick56007@hotmail.com>
2017-05-26 20:32:37 -0700

    update for heroku

--- bin/run_server.bat
+++ bin/run_server.bat
@@ -2,7 +2,7 @@
 
 cls
 
-call "C:\Program Files\Dart\dart-sdk\bin\dart.exe" C:\Users\derri\Documents\draw2\bin\server.dart --clientFiles "../build/web/"
+call "C:\Program Files\Dart\dart-sdk\bin\dart.exe" C:\Users\derri\Documents\draw2\bin\server\server.dart --clientFiles "../build/web/"
 
 pause
 pause
